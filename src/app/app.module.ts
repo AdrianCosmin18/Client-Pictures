8844dd51f9599342a835076bc8ff408e5b004061
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-06-10 14:09:25 +0300

    add rest code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadImageComponent } from './components/upload-image/upload-image.component';
+import { DownloadImageComponent } from './components/download-image/download-image.component';
+import {HttpClientModule} from "@angular/common/http";
+import { ImageListComponent } from './components/image-list/image-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImageComponent,
+    DownloadImageComponent,
+    ImageListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
